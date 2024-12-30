04df432b3f0cf0795ac1097af7e52adf7790b9f6
Daniel Zwart <Dtzwart@gmail.com>
2024-12-31 00:12:41 +0100

    bit more styling

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -8,8 +8,8 @@
     <title>Home Page</title>
     
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
-    <link rel="stylesheet" href="css/style_main.css">
     <link rel="stylesheet" href="css/style_header.css">
+    <link rel="stylesheet" href="css/style_main.css">
     <link rel="stylesheet" href="css/style_footer.css">
     <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,30 @@
 <div class="content">
-    <h1 class="text-center">Home</h1>
-    <p class="text-center">Welcome to the home page.</p>
+    <p style="text-align:center;font-size:20px">Welkom op de website van onze honderdjarige en springlevende padvindersgroep!</p>
+    <p>
+        <h4>Een echte padvindersgroep voor jongens vanaf 7 jaar</h4>
+        Met als uitvalsbasis een goed uitgerust clubhuis op een ruim anderhalve hectare groot terrein in de duinen van Overveen bieden wij jongens vanaf 7 jaar de tijd van hun leven!<br><br>
+        Elke zaterdag zijn onze leden in, maar vooral rond ons clubhuis en soms verder weg bezig met:
+        <ul style="list-style: none">
+            <li>Stoere spellen</li>
+            <li>Spannende avonturen</li>
+            <li>Uitdagende technieken</li>
+            <li>Samenwerken</li>
+            <li>Vaardigheden leren</li>
+        </ul>
+    </p>
+    <p>
+        <h4>Meedoen</h4>
+        Ben jij een jongen van 7 jaar of ouder en lijkt het je gaaf om mee te doen? Je kunt een keer komen kijken bij een van onze speltakken:
+        <ul style="list-style: none">
+            <li><a href="/welpen">Welpen</a> (7 - 11 jaar)</li>
+            <li><a href="/verkenners">Verkenners</a> (11 - 16 jaar)</li>
+            <li><a href="/rowans">Rowans</a> (16 - 18 jaar)</li>
+        </ul>
+    </p>
+    <p>
+        <h4>Kijk verder</h4>
+        Wil je meer weten over wie wij zijn? Kijk dan eens bij <a href="/groep">de groep</a> of <a href="/geschiedenis">de geschiedenis</a>.<br>
+        Onze groep maakt deel uit van de landelijke vereniging <a href ="https://www.scouting.nl/">Scouting Nederland</a> in de <a href="http://www.scoutinghaarlem.nl/">Regio Haarlem</a>.<br>
+        Ben je een scoutinggroep en heb je interesse in de huur van ons clubhuis voor je zomerkamp? Kijk op <a href="/verhuur">de verhuurpagina</a>.<br>
+    </p>
 </div>
