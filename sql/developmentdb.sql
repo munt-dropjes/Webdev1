4fe836417863425ddfa6e340565089076ff4fdd2
Daniel Zwart <Dtzwart@gmail.com>
2025-01-03 00:31:52 +0100

    Database update

--- sql/developmentdb.sql
+++ sql/developmentdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Nov 05, 2024 at 08:40 PM
+-- Generation Time: Jan 02, 2025 at 11:30 PM
 -- Server version: 11.5.2-MariaDB-ubu2404
 -- PHP Version: 8.2.23
 
@@ -72,7 +72,7 @@ CREATE TABLE `Roles` (
 
 INSERT INTO `Roles` (`id`, `role`) VALUES
 (1, 'admin'),
-(2, 'contentmanager');
+(2, 'content');
 
 -- --------------------------------------------------------
 
@@ -122,7 +122,7 @@ INSERT INTO `SpeltakInfo` (`id`, `speltakId`, `contactId`, `leeftijd`, `tijden`,
 (1, 2, 1, '7 tot 11 jaar', 'zaterdagmiddag van 14:30 tot 17:30 uur', 'Bij de speltak welpen zijn de jongens in de leeftijd van 7-11 jaar. De welpenhorde is opgedeeld in vijf subgroepjes die nesten genoemd worden.\r\n        Een nest bestaat uit een gids, een helper en nog 3-4 jongere welpen. Het leidingteam bestaat uit circa 6 man/vrouw.</p>\r\n        <img class=\"center\" id=\"width\" src=\"afbeeldingen/welpen/welpen_001.jpg\">\r\n        <p>Aan het begin van de middag wordt de horderoep gedaan. Daarna wordt er gestart met een spel om het energieoverschot kwijt te raken. \r\n        In principe gaan de welpen altijd naar buiten, op het eigen terrein of de duinen in. Het programma is zeer gevarieerd. \r\n        De ene keer wordt er vlaggenroof of levend kwartet gespeeld, de andere keer houden ze zich bezig met technieken zoals knopen, EHBO, kaart en kompas etc.</p>\r\n        <img class=\"center\" id=\"width\" src=\"afbeeldingen/welpen/welpen_002.jpg\">\r\n        <p>Een aantal keer in het seizoen is er een weekendkamp van zaterdag op zondag. Maar het hoogtepunt van het jaar is het zomerkamp.\r\n        De welpen verblijven dan een week lang in een clubhuis van een andere groep ergens in Nederland. Het zomerkamp is altijd in een bepaald thema zoals Robin Hood, Maffia of het Wilde Westen.</p>'),
 (2, 3, 1, '11 tot 16 jaar', 'zaterdagmiddag van 14:00 tot 18:00 uur', 'De verkenners zijn het logische vervolg op de welpen. Verkenner word je als je tussen de 11 en 15 jaar bent. Elke zaterdag wordt er vanaf 14\r\n      uur tot 18 uur opkomst gedraaid op ons terrein bij kraantje lek. De verkenners zijn onderverdeeld in 5 subgroepen (patrouilles) en staan onder\r\n      leiding van een stafteam van 6 personen. <br><br>\r\n      De verkenners houden zich bezig met de wat stoerdere padvinderstechnieken. Zo pionieren ze diverse dingen zoals een familieschommel of een reuzenrad.\r\n      Ook worden er geregeld touwbruggen en kabelbanen gemaakt, volgen ze een kaart en kompas route of gaan ze met zelf gebouwde vlotten het water op.\r\n      Daarnaast is er natuurlijk ook altijd tijd voor een potje sport en spel.<br><br>\r\n      Drie maal per jaar hebben de verkenners een weekend. Dan slapen ze soms in een tent, soms op een zelf gebouwd stapelbed maar ook wel eens in de Limburgse mergelgroeve.<br><br>\r\n      Aan het einde van het seizoen gaan de verkenners twee weken op zomerkamp. Normaal gesproken kamperen ze dan ergens in Nederland maar eens in de vier jaar vertrekken\r\n      de verkenners naar Schotland om daar te kamperen.<br><br>'),
 (3, 4, 1, 'vanaf 16 jaar', 'zaterdagmiddag van 13:00 tot ongeveer 22:00 uur', 'Als je te oud bent geworden om nog een heel nieuw seizoen bij de verkenners te blijven dan maken veel verkenners de keuze om \'over te vliegen\' \r\n      naar de rowans. Deze kleinste speltak van onze groep is bedoeld voor jongens tussen de 16 & 18 jaar. Het idee is dat je dan zonder verdere begeleiding, \r\n      hooguit wat aansturing van een enkele volwassene, je eigen dagprogramma verzint en uitvoert. Meestal gaat dit zonder problemen omdat de rowans elkaar \r\n      vaak al van jongs af aan kennen.</p>\r\n      <p>De programma&#39;s die bedacht worden, zijn vaak pionieren en trappersbanen bouwen, met als groot verschil dat het allemaal wat heftiger en extremer \r\n      is dan hoe het bij de verkenners gedaan werd. De kennis die je als verkenner opgedaan hebt, ga je als rowan gebruiken om nieuwe dingen te verzinnen \r\n      of andere dingen te perfectioneren. Vaak kom je ook voor een probleem te staan omdat het dan niet helemaal zo loopt zoals je van plan was, als rowan \r\n      moet je dan instaat zijn om het op je eigen manier zo goed en veilig mogelijk op te lossen.</p>\r\n      <p>Een ander niet te vergeten taak van de rowans is onderhoud aan clubhuis en soms ook aan het terrein. Als rowan heb je namelijk je eigen clubhuis \r\n      (TNO) waarvan de rowans door de jaren heen hun eigen hanghok hebben gemaakt. Je bent dan ook vrij om zelf te bepalen wanneer je blijft slapen.</p>\r\n      <img class=\"center\" id=\"width\" src=\"afbeeldingen/rowans/rowans_001.jpg\">\r\n      <p class=\"img-p\">Zomerkamp: kano&#235;n in midden Zweden</p>\r\n      <p>Bij de verkenners is het vanzelf sprekend dat je aan het eind van het seizoen voor twee weken op zomerkamp gaat en meestal aan het begin van de vakantie. \r\n      Omdat je als rowan de dingen zelf mag bepalen mag je ook zelf bepalen of je op zomerkamp gaat en wanneer. Je kunt je inschrijven voor een gezamenlijke kamp \r\n      in het buitenland waar je veel andere mensen leert kennen of je besluit met de hele speltak te gaan trekken door de wildernis in bijvoorbeeld Scandinavi&#235; of Schotland.</p>\r\n      <img class=\"center\" id=\"width\" src=\"afbeeldingen/rowans/rowans_002.JPG\">\r\n      <p class=\"img-p\">Programma op zaterdag: schommelbad</p>\r\n      <p>Het is zeer belangrijk dat er een goede en leuke groep rowans blijft bestaan want deze mensen worden later vaak welpenleiding of staf van de verkenners.</p>'),
-(4, 8, NULL, '', '', 'p><h4>De stam</h4>De stam van de Camerons - Duinzwervers is in 1997 opgericht door een zestal oud-leidinggevenden.</p>\r\n      <p>De stam is een speltak waarbinnen de oud-leidinggevenden nog in zekere mate actief zijn binnen de groep. Zij zijn niet meer wekelijks actief als staf of leiding, maar zij verlenen onder andere ondersteuning bij groepsactiviteiten zoals de nieuwjaarsopkomst, weekendkampen, een re&#252;nie, zomerkampen, enzovoorts. Daarbij  verrichten enkele stamleden bestuurlijke taken.</p>\r\n      <p>Buiten de bovengenoemde ondersteuning, draait de stam ook eigen programma&#39;s. Deze opkomsten worden gehouden op de eerste zaterdag van de oneven maanden. De opkomsten zijn zeer divers. Hierbij valt te denken aan zeilen, GPS-tochten, whiskyproeverijen, maar ook pionieren en vlotten bouwen. </p>\r\n      <p>In de historie van de Camerons - Duinzwervers is na het Schotlandkamp van 1973 de bijzonderheid ontstaan dat elke speltak een eigen das en een eigen kleur uniform draagt. Bij de oprichting van de stam is deze lijn doorgetrokken. De stam draagt als das de &quot;Cameron Hunting&quot;. Deze groenblauwe das wordt gedragen op een donkergroen uniform.</p>');
+(4, 8, NULL, '', '', '<p><h4>De stam</h4>De stam van de Camerons - Duinzwervers is in 1997 opgericht door een zestal oud-leidinggevenden.</p>\r\n      <p>De stam is een speltak waarbinnen de oud-leidinggevenden nog in zekere mate actief zijn binnen de groep. Zij zijn niet meer wekelijks actief als staf of leiding, maar zij verlenen onder andere ondersteuning bij groepsactiviteiten zoals de nieuwjaarsopkomst, weekendkampen, een re&#252;nie, zomerkampen, enzovoorts. Daarbij  verrichten enkele stamleden bestuurlijke taken.</p>\r\n      <p>Buiten de bovengenoemde ondersteuning, draait de stam ook eigen programma&#39;s. Deze opkomsten worden gehouden op de eerste zaterdag van de oneven maanden. De opkomsten zijn zeer divers. Hierbij valt te denken aan zeilen, GPS-tochten, whiskyproeverijen, maar ook pionieren en vlotten bouwen. </p>\r\n      <p>In de historie van de Camerons - Duinzwervers is na het Schotlandkamp van 1973 de bijzonderheid ontstaan dat elke speltak een eigen das en een eigen kleur uniform draagt. Bij de oprichting van de stam is deze lijn doorgetrokken. De stam draagt als das de &quot;Cameron Hunting&quot;. Deze groenblauwe das wordt gedragen op een donkergroen uniform.</p>');
 
 -- --------------------------------------------------------
 
@@ -143,7 +143,28 @@ CREATE TABLE `Users` (
 --
 
 INSERT INTO `Users` (`id`, `roleId`, `name`, `email`, `password`) VALUES
-(1, 0, 'daniel', 'daniel@daniel.nl', '$2y$10$EkEYgS9xEPHuK7RNEVoQr.0VC0CXsrx/e.9NK9Oyi9G6pQ8IdyqnO');
+(1, 1, 'daniel', 'daniel@daniel.nl', '$2y$10$EkEYgS9xEPHuK7RNEVoQr.0VC0CXsrx/e.9NK9Oyi9G6pQ8IdyqnO');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `VerhuurInfo`
+--
+
+CREATE TABLE `VerhuurInfo` (
+  `id` int(11) NOT NULL,
+  `beginDatum` varchar(255) NOT NULL,
+  `eindDatum` varchar(255) NOT NULL,
+  `beschikbaar` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `VerhuurInfo`
+--
+
+INSERT INTO `VerhuurInfo` (`id`, `beginDatum`, `eindDatum`, `beschikbaar`) VALUES
+(1, '8 juli', '15 juli', 1),
+(2, '15 juli', '22 juli', 0);
 
 --
 -- Indexes for dumped tables
@@ -180,6 +201,13 @@ ALTER TABLE `SpeltakInfo`
 -- Indexes for table `Users`
 --
 ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `roleId` (`roleId`);
+
+--
+-- Indexes for table `VerhuurInfo`
+--
+ALTER TABLE `VerhuurInfo`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -191,6 +219,12 @@ ALTER TABLE `Users`
 --
 ALTER TABLE `ContactInfo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Roles`
+--
+ALTER TABLE `Roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `Speltak`
@@ -211,6 +245,12 @@ ALTER TABLE `Users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `VerhuurInfo`
+--
+ALTER TABLE `VerhuurInfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -226,6 +266,12 @@ ALTER TABLE `ContactInfo`
 ALTER TABLE `SpeltakInfo`
   ADD CONSTRAINT `SpeltakInfo_ibfk_1` FOREIGN KEY (`speltakId`) REFERENCES `Speltak` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `SpeltakInfo_ibfk_2` FOREIGN KEY (`contactId`) REFERENCES `ContactInfo` (`id`);
+
+--
+-- Constraints for table `Users`
+--
+ALTER TABLE `Users`
+  ADD CONSTRAINT `Users_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `Roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
