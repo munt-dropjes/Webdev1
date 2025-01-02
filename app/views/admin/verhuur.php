77ab8ad150f7cd9b51b1c23d311368870cfc0023
Daniel Zwart <Dtzwart@gmail.com>
2025-01-03 00:29:42 +0100

    admin verhuur fix

--- app/views/admin/verhuur.php
+++ app/views/admin/verhuur.php
@@ -1,23 +1,27 @@
-<div>
-        <table class="verhuur">
+<?php
+    require_once 'admin-navigation.php';
+?>
+
+<div class="content">
+    <table class="verhuur">
+        <tr class="verhuur">
+            <th class="verhuur">Verhuurweek</th>
+            <th class="verhuur">Begin datum</th>
+            <th class="verhuur">Eind datum</th>
+            <th class="verhuur">Beschikbaarheid</th>
+        </tr>
+        <?php foreach ($verhuurInfo->verhuurData as $verhuurData) : ?>
             <tr class="verhuur">
-                <th class="verhuur">Verhuurweek</th>
-                <th class="verhuur">Begin datum</th>
-                <th class="verhuur">Eind datum</th>
-                <th class="verhuur">Beschikbaarheid</th>
+                <td class="verhuur"> <?=$verhuurData->verhuurWeek?> </td>
+                <td class="verhuur"> <?=$verhuurData->startDatum?> </td>
+                <td class="verhuur"> <?=$verhuurData->eindDatum?> </td>
+                <td class="verhuur"> <?php
+                    if ($verhuurData->beschikbaarheid)
+                        echo "Beschikbaar";
+                    else 
+                        echo "Niet beschikbaar";                
+                ?> </td>
             </tr>
-            <?php foreach ($verhuurInfo->verhuurData as $verhuurData) : ?>
-                <tr class="verhuur">
-                    <td class="verhuur"> <?=$verhuurData->verhuurWeek?> </td>
-                    <td class="verhuur"> <?=$verhuurData->startDatum?> </td>
-                    <td class="verhuur"> <?=$verhuurData->eindDatum?> </td>
-                    <td class="verhuur"> <?php
-                        if ($verhuurData->beschikbaarheid)
-                            echo "Beschikbaar";
-                        else 
-                            echo "Niet beschikbaar";                
-                    ?> </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </div>
+        <?php endforeach; ?>
+    </table>
+</div>
